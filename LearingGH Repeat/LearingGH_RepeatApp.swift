0dc12cbe3f83c2ad0ab6cfe4fd46e3b7298604e0
Am Samra <amamsamra@gmail.com>
2021-08-15 21:05:35 +0100

    Initial Commit

---
+++ LearingGH Repeat/LearingGH_RepeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearingGH_RepeatApp.swift
+//  LearingGH Repeat
+//
+//  Created by Amarjit Samra on 15/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearingGH_RepeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
